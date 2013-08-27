cc33bfe96c0fc873a9f1d909c45a2028c17cb003
Kyle Reierson <klreierson@yahoo.com>
2013-08-26 21:19:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickerface::Application.config.secret_key_base = 'c9ba4e598f8d9240686c59aa6ad48eb387ff4ab6fc1d92ffcc608c826a2a48a1fea3178e76e3cd0e92c2a214b4b27063e07d3bc53e43553285ea39039081e127'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tickerface::Application.config.session_store :cookie_store, key: '_tickerface_session'
